6acb7d0fa3364ade2a85eb64f8ff9c07b329fccf
fatouuuu <fatmaabdullrahim@gmail.com>
2023-02-27 12:28:54 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,13 @@
-class Dog
+require 'active_record'
+
+# establish databse connection
+ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    databse: 'test.sqlite3'
+)
+
+
+# Establish database connection 
+class Dog < ActiveRecord::Base 
+
 end
